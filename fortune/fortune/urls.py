a3fbfea2bef1616d372c0507bb2768f47d03df7a
goldsoft1206 <icbsoft87@gmail.com>
2017-01-10 05:13:21 +0800

    removed django.contrib.admin from project

--- fortune/fortune/urls.py
+++ fortune/fortune/urls.py
@@ -18,6 +18,5 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
     url(r'^', include('userprofile.urls', namespace='users')),
 ]
