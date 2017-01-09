ffde1fb71e5546d72a8e12d6119126ce120d8608
goldsoft1206 <icbsoft87@gmail.com>
2017-01-10 02:09:29 +0800

    defined urls/views for UserList

--- fortune/userprofile/views.py
+++ fortune/userprofile/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import UserProfile
+
+
+class UserListView(ListView):
+	"""
+	View to list all users
+	"""
+	model = UserProfile
+	template_name = "users/list.html"
