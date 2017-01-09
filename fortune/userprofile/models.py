ac9c27c5bf87dc01d9ad64a872a0025dc18080b9
goldsoft1206 <icbsoft87@gmail.com>
2017-01-10 01:53:18 +0800

    defined UserProfile model

--- fortune/userprofile/models.py
+++ fortune/userprofile/models.py
@@ -1,5 +1,17 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserProfile(models.Model):
+	"""
+	UserProfile model extends User model for birthdate and random number
+	"""
+	user = models.OneToOneField(User, related_name="profile")
+
+	birth_date = models.DateField()
+
+	# random number should be range(1, 100)
+	randome_number = models.PositiveSmallIntegerField(default=1)
+
