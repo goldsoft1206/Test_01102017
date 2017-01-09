31b343eeb7952cc39748245b6758c87a255239a6
goldsoft1206 <icbsoft87@gmail.com>
2017-01-10 01:53:10 +0800

    added userprofile app to INSTALLED_APPS

--- fortune/fortune/settings.py
+++ fortune/fortune/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # self developed apps
+    'userprofile',
 ]
 
 MIDDLEWARE_CLASSES = [
